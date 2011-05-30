04d88774d2d72786263996ec15ecbe360b6eb796
Johannes Baiter <johannes.baiter@gmail.com>
2011-05-30 12:38:09 +0200

    added get_clean_name and started work on player api

--- kinoknecht/api.py
+++ kinoknecht/api.py
@@ -1,22 +1,21 @@
 from __future__ import absolute_import
 
+import os
+import re
+
+from imdb import IMDb
 from simpleapi import Namespace, serialize
 
+import config
 from kinoknecht.helpers import CATEGORIES
 from kinoknecht.database import db_session
 from kinoknecht.models import Videofile, Movie, Show, Episode
 from kinoknecht.player import Player
 
-
+imdb = IMDb()
+player = Player(config.extra_args)
 
 class DBApi(Namespace):
-    def query_imdb(searchstr):
-        results = [dict(imdbid=entry.movieID,
-            title=entry['long imdb canonical title'])
-            for entry in i.search_movie(searchstr)]
-        return serialize(results[0:9])
-    query_imdb.published = True
-
     def create(category, vfiles, imdbid=None, title=None):
         if not imdbid and not title:
             return "Please specifiy either a title or an imdb id!"
@@ -29,19 +28,21 @@ class DBApi(Namespace):
         obj = None
         if category == 'movie':
             obj = Movie(videofiles=vfiles)
-            try: mov.imdb_id = int(imdbid)
-            except NameError: pass
         elif category == 'show':
             obj = Show()
-            try: show.title = title
-            except NameError: pass
         elif category == 'episode':
-            episode = Episode(vfiles[0])
-        db_session.add(mov)
+            obj = Episode(vfiles[0])
+
+        try: obj.title = title
+        except NameError: pass
+        try: obj.imdb_id = int(imdbid)
+        except NameError: pass
+
+        db_session.add(obj)
         db_session.commit()
         return obj.id
     create.published = True
-    
+
     def query(category, searchstr):
         """Simple query for objects by name"""
         try: objtype = CATEGORIES[category]
@@ -60,3 +61,66 @@ class DBApi(Namespace):
             show.episodes.append(epi)
         return True
     add_to_show.published = True
+
+    def query_imdb(searchstr):
+        results = [dict(imdbid=entry.movieID,
+            title=entry['long imdb canonical title'])
+            for entry in imdb.search_movie(searchstr)]
+        return serialize(results[0:9])
+    query_imdb.published = True
+
+    def get_clean_name(fname=None, vfid=None):
+        #TODO: Move this to a Videofile method?
+        if not fname and not vfid:
+            raise Exception(
+                    'Insufficient arguments, specify either fname or vfid')
+        # For files that comply to scene filenaming "standards"
+        scene_rexp = re.compile(r'([\w\s]+)( \d{4})? (.+Rip) .*', re.I)
+        # That is one nasty sunnufabitch...
+        rexp = re.compile(r'(?:\d{4}\s*\-\s*?)?(?:[\w\s]*-\s*)?([\w\s\.\-]+)(?:\(?\d{4}\)?)?.*', re.I)
+        if not fname:
+            fname = Videofile.get(int(vfid)).name
+        # Normalize the name by removing the extension and all dots
+        fname = os.path.splitext(fname)[0].replace('.', ' ')
+        # Do we have a scene filename?
+        fname_match = scene_rexp.match(fname)
+        if fname_match:
+            return fname_match.groups()[0].strip()
+        # Doesn't look like it, let's be more fuzzy
+        fname_match = rexp.match(fname)
+        if fname_match:
+            return fname_match.groups()[0].strip()
+        # We give up and just return the normalized name
+        else:
+            return fname
+    get_clean_name.published = True
+
+class PlayerApi(Namespace):
+    def play(category, id):
+        vfile = Videofile.get(id)
+        player.loadfile(os.path.join(vfile.path, vfile.name))
+        return True
+    play.published = True
+
+    def pause():
+        player.pause()
+        return True
+    pause.published = True
+
+    def stop():
+        player.stop()
+        return True
+    stop.published = True
+
+    def seek(position):
+        player.seek(position)
+        return True
+    seek.published = True
+
+    def load_subtitle(subid):
+        subpath = os.path.abspath(Videofile.subfilepaths[subid])
+        player.sub_load(subpath)
+
+    def get_position():
+        return player.time_pos
+
